737f9f132b65a127de5d900eb7fd752c89a5d1bd
Timothy Adamson <MrTimcakes@gmail.com>
2019-11-01 21:40:04 +0000

    Intial API Call

---
+++ app.js
@@ -0,0 +1,20 @@
+let url = "http://ergast.com/api/f1/current/next.json";
+
+
+fetch(url)
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
