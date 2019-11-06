af97c071b9ba837f116b9aed17af43b7d592a5c6
Timothy Adamson <MrTimcakes@gmail.com>
2019-11-06 16:59:07 +0000

    Fetch User Defined Data from the API

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 let url = "http://ergast.com/api/f1/current/next.json";
 
 
-fetch(url)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+var SeasonButton = document.getElementById("SeasonButton");
+var SeasonInput = document.getElementById("SeasonInputID");
+SeasonButton.addEventListener("click", displaySeasonData, false);
+SeasonInput.addEventListener("keyup", function(e) { if (e.keyCode === 13) {e.preventDefault();SeasonButton.click();} });
 
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+async function displaySeasonData(){
+  let season = parseInt(SeasonInput.value);
+  let data = await getSeasonData(season);
+  //console.log(season);
+  console.log(data);
+  document.getElementById("test").textContent = JSON.stringify(data, null, "\t");
+}
+
+
+async function getSeasonData(season){
+  let requestURL = `http://ergast.com/api/f1/${season}.json`;
+  let request = await fetch(requestURL);
+  let json = await request.json();
+
+  //console.log(json);
+  return json;
+}
