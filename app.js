87ca824464464dfe68378f5c95f1c1383702a05a
Timothy Adamson <MrTimcakes@gmail.com>
2019-11-20 18:54:10 +0000

    Load weather data for upcoming circuit

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 let url = "//ergast.com/api/f1/current/next.json";
 var test1, test2;
 
-var SeasonButton = document.getElementById("SeasonButton");
-var SeasonInput = document.getElementById("SeasonInputID");
-SeasonButton.addEventListener("click", displaySeasonData, false);
-SeasonInput.addEventListener("keyup", function(e) { if (e.keyCode === 13) {e.preventDefault();SeasonButton.click();} });
-
 async function displaySeasonData(){
   let season = parseInt(SeasonInput.value);
   let RaceTable = document.getElementById("RaceTable");
@@ -34,8 +29,8 @@ async function displaySeasonData(){
 
     RaceTable.appendChild(raceDiv);
 
-    console.log()
-    console.log(races[race]);
+    //console.log()
+    //console.log(races[race]);
     // document.getElementById("RaceTable").textContent = JSON.stringify(races[race], null, "\t");
   }
 
@@ -46,8 +41,38 @@ async function displaySeasonData(){
   // }, document.getElementById('RaceTable'))
 
   //console.log(season);
-  console.log(data);
+  //console.log(data);
   // document.getElementById("test").textContent = JSON.stringify(data, null, "\t");
+}
+
+async function displayNextCircuitData(){
+  let season = parseInt(SeasonInput.value);
+  let RaceTable = document.getElementById("RaceTable");
+  let data = await getSeasonData(season + "/next");
+  let races = data.MRData.RaceTable.Races;
+
+  RaceTable.textContent = "";
+
+  for(let race in races){
+    var raceDiv = document.createElement("div");
+    raceDiv.className = "race"
+        
+    Object.keys(races[race]).forEach((key)=>{
+      if(key=="url"){return;}
+      let newEle = document.createElement("div");
+      switch(key){
+        case "Circuit":
+            newEle.textContent = `${key}: ${races[race][key]["circuitName"]}`;
+            break;
+        default:
+            newEle.textContent = `${key}: ${races[race][key]}`;
+            break;
+      }
+      raceDiv.appendChild(newEle);
+    });
+
+    RaceTable.appendChild(raceDiv);
+  }
 }
 
 async function getSeasonData(season){
@@ -57,4 +82,82 @@ async function getSeasonData(season){
   return json;
 }
 
-SeasonButton.click();
+
+var loadJS = function(url, implementationCode, location){
+  //url is URL of external file, implementationCode is the code
+  //to be called from the file, location is the location to 
+  //insert the <script> element
+
+  var scriptTag = document.createElement('script');
+  scriptTag.src = url;
+
+  scriptTag.onload = implementationCode;
+  scriptTag.onreadystatechange = implementationCode;
+
+  location.appendChild(scriptTag);
+};
+var yourCodeToBeCalled = function(){
+//your code goes here
+}
+
+
+function displayWeather(parent, lat, long, name){
+  // let url = `//forecast.io/embed/#lat=${lat}&lon=${long}&name=${name}&color=#00aaff`;
+  let url = `//darksky.net/widget/default/${lat},${long}/us12/en.js?width=100%&height=350&title=${name}&textColor=333333&bgColor=FFFFFF&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=yes`;
+  // loadJS(url, ()=>{console.log("test")}, parent);
+  let weather = document.createElement("script")
+  weather.src = url;
+  parent.appendChild(weather);
+}
+
+function displayRaces(parent, data){
+  let RaceTable = parent;
+  //let data = await getSeasonData(season + "/next");
+  let races = data.MRData.RaceTable.Races;
+
+  RaceTable.textContent = "";
+
+  for(let race in races){
+    var raceDiv = document.createElement("div");
+    raceDiv.className = "race"
+        
+    Object.keys(races[race]).forEach((key)=>{
+      if(key=="url"){return;}
+      let newEle = document.createElement("div");
+      switch(key){
+        case "Circuit":
+            newEle.textContent = `${key}: ${races[race][key]["circuitName"]}`;
+            break;
+        default:
+            newEle.textContent = `${key}: ${races[race][key]}`;
+            break;
+      }
+      raceDiv.appendChild(newEle);
+    });
+
+    RaceTable.appendChild(raceDiv);
+  }
+}
+
+window.addEventListener('load', async function() {
+  console.log('All assets are loaded')
+  // var SeasonInput = document.getElementById("SeasonInputID");
+  // var SeasonButton = document.getElementById("SeasonButton");
+  // var nextCircuitButton = document.getElementById("nextCircuitButton");
+  // SeasonInput.addEventListener("keyup", function(e) { if (e.keyCode === 13) {e.preventDefault();SeasonButton.click();} });
+  // SeasonButton.addEventListener("click", displaySeasonData, false);
+  // nextCircuitButton.addEventListener("click", displayNextCircuitData, false);
+  // SeasonButton.click();
+  season = "current";
+  seasonData = await getSeasonData(season)
+  //console.log(seasonData)
+  next = await getSeasonData(season+"/next")
+  date = next.MRData.RaceTable.Races[0].date;
+  circuitLoc = next.MRData.RaceTable.Races[0].Circuit.Location
+  circuitName = next.MRData.RaceTable.Races[0].Circuit.circuitName
+  displayWeather( document.getElementById("weatherDiv"), circuitLoc.lat, circuitLoc.long, circuitName )
+  displayRaces(document.getElementById("schedule"), seasonData)
+  //console.log( `//forecast.io/embed/#lat=${circuitLoc.lat}&lon=${circuitLoc.long}&name=${circuitName}&color=#00aaff` )
+  
+  console.log(date)
+})
